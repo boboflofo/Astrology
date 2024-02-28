23282895a1510042e7965fc11cac0494ea303c01
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-02-28 08:44:01 -0800

    adding some new logic for crystal ball

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import '../App.css'
+import '../Game.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
