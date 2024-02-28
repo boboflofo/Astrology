2572c3bf45a9f1e5663730fbfbe36dc9b591822d
Brianca Knight <briancaknight@gmail.com>
2024-02-28 15:48:51 -0800

    revert Game.jsx

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -23,6 +23,8 @@ const GameForm = () => {
     }
   ];
 
+  
+
   const handleOptionClick = (option) => {
     setUserAnswers([...userAnswers, option]);
     if (currentQuestionIndex < questions.length - 1) {
@@ -37,7 +39,7 @@ const GameForm = () => {
     try {
       const response = await fetch(`https://localhost:5001/api/signs?expression=${userAnswers[0]}&description=${userAnswers[1]}&concentration=${userAnswers[2]}`);
       const jsonResponse = await response.json();
-      setAstrologyData(jsonResponse)
+
       if (!response.ok) {
         const errorMessage = `Error fetching astro data: ${response.status} ${response.statusText} ${jsonResponse.message}`;
         throw new Error(errorMessage);
@@ -48,11 +50,6 @@ const GameForm = () => {
       console.error('Error fetching astro data', error);
     }
   };
-  
-      
-    
-    
-    
 
   const handlePlayAgain = () => {
     setCurrentQuestionIndex(0);
@@ -97,90 +94,6 @@ const GameForm = () => {
           </div>
         </div>
       </div>
-
-
-      <div className='form-container'>
-        <form onSubmit={handleFormSubmit}>
-          <h2>Would you describe yourself as more...</h2>
-
-          <label>
-            <input type="radio" value="Introverted" onChange={() => setUserExpression('Introverted')} />Introverted
-          </label>
-
-          <label>
-            <input type="radio" value="Extroverted" onChange={() => setUserExpression('Extroverted')} />Extroverted
-          </label>
-
-
-          <h2>You are more...</h2>
-
-          <label>
-            <input type="radio" value="Practical" onChange={() => setUserDescription('Practical')} />Practical
-          </label>
-
-          <label>
-            <input type="radio" value="Emotional" onChange={() => setUserDescription('Emotional')} />Emotional
-          </label>
-
-          <label>
-            <input type="radio" value="Social" onChange={() => setUserDescription('Social')} />Social
-          </label>
-
-          <label>
-            <input type="radio" value="Idealistic" onChange={() => setUserDescription('Idealistic')} />Idealistic
-          </label>
-
-          <h2>You concentrate more on...</h2>
-
-          <div>
-            <span
-              className={userConcentration === 'Action' ? 'selected' : ''}
-              onClick={() => setUserConcentration('Action')}
-            >
-              Action
-            </span>
-            <span
-              className={userConcentration === 'Preservation' ? 'selected' : ''}
-              onClick={() => setUserConcentration('Preservation')}
-            >
-              Preservation
-            </span>
-            <span
-              className={userConcentration === 'Learning' ? 'selected' : ''}
-              onClick={() => setUserConcentration('Learning')}
-            >
-              Learning
-            </span>
-          </div>
-          {/* <label>
-            <input type="radio" value="Action" onChange={() => setUserConcentration('Action')} />Action
-          </label>
-
-          <label>
-            <input type="radio" value="Preservation" onChange={() => setUserConcentration('Preservation')} />Preservation
-          </label>
-
-          <label>
-            <input type="radio" value="Learning" onChange={() => setUserConcentration('Learning')} />Learning
-          </label> */}
-
-          <br />
-          <br />
-          <button type="submit">Submit</button>
-
-        </form>
-
-
-        {astrologyData && (
-          <div>
-            <h2>Your crystal ball reveals: </h2>
-            <br />
-            <h3>You are a... </h3>
-            <h1>{astrologyData}</h1>
-          </div>
-        )}
-      </div>
-      
     </div>
   );
 };
