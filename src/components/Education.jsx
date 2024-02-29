29813068053fce5d80281ef6c49145db9d93cae3
Jonathan Song <jonats10@uci.edu>
2024-02-29 13:52:10 -0800

    add back card image

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Container, Row, Card } from 'react-bootstrap'
+import Aries from "../assets/Aries.jpg"
 
 import '../App.css'
 
@@ -9,73 +10,73 @@ function Education({ selectedSign }) {
       sign: "Aries",
       dates: "March 21 - April 19",
       traits: ["Courageous", "Confident", "Optimistic", "Passionate", "Impulsive"],
-      imageUrl: ""
+      imageUrl: "src/assets/Aries.jpg"
     },
     { 
       sign: "Taurus",
       dates: "April 20 - May 20",
       traits: ["Patient", "Dependable", "Persistent", "Practical", "Generous"],
-      imageUrl: ""
+      imageUrl: "src/assets/Taurus.jpg"
     },
     { 
       sign: "Gemini",
       dates: "May 21 - June 20",
       traits: ["Adaptable", "Versatile", "Energetic", "Curious", "Communicative"],
-      imageUrl: ""
+      imageUrl: "src/assets/Gem.jpg"
     },
     { 
       sign: "Cancer",
       dates: "June 21 - July 22",
       traits: ["Loyal", "Emotional", "Sympathetic", "Intuitive", "Protective"],
-      imageUrl: ""
+      imageUrl: "src/assets/Cancer.jpg"
     },
     { 
       sign: "Leo",
       dates: "July 23 - August 22",
       traits: ["Confident", "Ambitious", "Generous", "Loyal", "Determined"],
-      imageUrl: ""
+      imageUrl: "src/assets/Leo.jpg"
     },
     { 
       sign: "Virgo",
       dates: "August 23 - September 22",
       traits: ["Practical", "Analytical", "Kind", "Hardworking", "Loyal"],
-      imageUrl: ""
+      imageUrl: "src/assets/Virgo.jpg"
     },
     { 
       sign: "Libra",
       dates: "September 23 - October 22",
       traits: ["Diplomatic", "Charming", "Social", "Fair", "Cooperative"],
-      imageUrl: ""
+      imageUrl: "src/assets/Libra.jpg"
     },
     { 
       sign: "Scorpio",
       dates: "October 23 - November 21",
       traits: ["Passionate", "Resourceful", "Brave", "Stubborn", "Determined"],
-      imageUrl: ""
+      imageUrl: "src/assets/Scorpio.jpg"
     },
     { 
       sign: "Sagittarius",
       dates: "November 22 - December 21",
       traits: ["Optimistic", "Generous", "Adventurous", "Philosophical", "Careless"],
-      imageUrl: ""
+      imageUrl: "src/assets/Sag.jpg"
     },
     { 
       sign: "Capricorn",
       dates: "December 22 - January 19",
       traits: ["Responsible", "Disciplined", "Self-controlled", "Good Managerial Skills", "Practical"],
-      imageUrl: ""
+      imageUrl: "src/assets/Cap.jpg"
     },
     { 
       sign: "Aquarius",
       dates: "January 20 - February 18",
       traits: ["Independent", "Progressive", "Humanitarian", "Intellectual", "Unpredictable"],
-      imageUrl: ""
+      imageUrl: "src/assets/Aqua.jpg"
     },
     { 
       sign: "Pisces",
       dates: "February 19 - March 20",
       traits: ["Compassionate", "Adaptable", "Accepting", "Imaginative", "Sensitive"],
-      imageUrl: ""
+      imageUrl: "src/assets/Pisces.jpg"
     }
   ])
 
@@ -121,7 +122,11 @@ function Education({ selectedSign }) {
                     </div>
                   </Card.Text>
                 ) : (
-                  <div className="back-image" onClick={() => toggleDisplay(index)} style={{ backgroundImage: `url(${sign.imageUrl})` }}></div>
+                  <div className="back-image" onClick={() => toggleDisplay(index)} style={{ backgroundImage: `url(${sign.imageUrl})` }}>
+                    <Card.Img variant='top' src={sign.imageUrl} />
+                  </div>
+                  
+
                 )}
               </Card.Body>
             </Card>
